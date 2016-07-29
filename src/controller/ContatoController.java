950dcb7ed60d31e00919c52ae342f1f7982fd51c
a9211062 <a9211062@02LAB108PC12.rn.senac.br>
2016-07-29 18:26:58 -0300

    1 comint

---
+++ src/controller/ContatoController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import dao.ContatoDAO;
+import java.util.List;
+import model.Contato;
+
+public class ContatoController {
+
+    private ContatoDAO dao;
+
+    public ContatoController() {
+        dao = new ContatoDAO();
+    }
+    public boolean adicionar(String nome, String fone){
+        Contato contato = new Contato();
+        contato.setNome(nome.toUpperCase());
+        contato.setFone(fone);
+        
+        return dao.inset(contato);
+    }
+    public boolean atualizar(int id, String nome, String fone){
+    Contato contato = new Contato();
+        contato.setId(id);
+        contato.setNome(nome.toUpperCase());
+        contato.setFone(fone);        
+        return dao.update(contato);
+    }
+    
+    public List<Contato> listar(String filter){
+        if(filter == null){
+        return dao.selectALL();
+        }else{
+            return dao.selectFilter(filter);
+        }
+        
+    }
+
+   
+    
+}
